9f85286f92e13b5e895061ffefbe5e8fd840d624
Byron Wezvo <byronwezvo@outlook.com>
2021-08-20 18:39:42 +0200

    cleaned repo

--- Sucasa/src/utils/format-date.jsx
+++
@@ -1,6 +0,0 @@
-import moment from "../pages/dob/dob";
-
-
-export default function FormatDate(){
-// return    moment(selectedYear).format('YYYY') + '-' + moment(selectedMonth).format('MM')  + '-' + moment(selectedDate).format('DD');
-}
